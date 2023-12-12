17c2b8be692b650b3dee892989d048318e078b19
curtyraissa <raissacurty@gmail.com>
2023-12-12 15:59:51 +0000

    chore: Finalize header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/ignite-simbol.png'
 
 export function Header(){
     return(
-        <header className={styles.header}>Ignite Feed</header>
+        <header className={styles.header}>
+            <img src={igniteLogo} alt="logotipo ignit"/>
+        </header>
     )
 }
