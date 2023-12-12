c5dae5289b4521f6cbba425b036147b40f00a1ea
curtyraissa <raissacurty@gmail.com>
2023-12-12 16:33:59 +0000

    feat: Create sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { Header } from './components/Header'
 import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
     <>
     <Header/>
+    <div className={styles.wrapper}>
+      <Sidebar/>
+      {/* <main>
+      <Post/>
+      </main> */}
+    </div>
     </>
   )
 }
