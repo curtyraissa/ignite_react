3119e97e9bd58b362919a423d2b149eb43c473b7
curtyraissa <raissacurty@gmail.com>
2023-12-13 15:43:38 +0000

    chore: complete sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -8,12 +9,14 @@ export function Sidebar(){
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
         <div className={styles.profile}>
+            <img src='https://avatars.githubusercontent.com/u/117386154?v=4' alt='avatar'/>
             <strong>Raissa Curty</strong>
             <span>Web Developer</span>
         </div>
         <footer>
             <a href='#'>
-                Editar seu Perfil
+                <PencilLine size={20}/>
+                Editar seu perfil
             </a>
         </footer>
         </aside>
