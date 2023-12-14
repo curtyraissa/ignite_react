df73f110075ecd90930950164588c7eae1af341f
curtyraissa <raissacurty@gmail.com>
2023-12-14 19:47:39 +0000

    feat: Add comments section to post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,24 +1,41 @@
-import styles from './Post.module.css'
+import styles from "./Post.module.css";
 
-export function Post(){
-    return(
-        <article className={styles.post}>
-            <header>
-                <div className={styles.author}>
-                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/117386154?v=4"/>
-                    <div className={styles.authorInfo}>
-                        <strong>Raissa Curty</strong>
-                        <span>Web Developer</span>
-                    </div>
-                </div>
-                <time title="13 de Dez às 17:00" dateTime="2023-12-13 17:00">Publicado há 1h</time>
-            </header>
-            <div className={styles.content}>
-                <p>Lorem ipsum dolor sit amet</p>
-                <p>Lorem ipsum dolor sit amet</p>
-                <p><a href="">github/curtyraissa</a></p>
-                <p><a href="">#novoprojeto</a></p>
-            </div>
-        </article>
-    )
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/117386154?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Raissa Curty</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title="13 de Dez às 17:00" dateTime="2023-12-13 17:00">
+          Publicado há 1h
+        </time>
+      </header>
+      <div className={styles.content}>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>Lorem ipsum dolor sit amet</p>
+        <p>
+          <a href="">github/curtyraissa</a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
+  );
 }
