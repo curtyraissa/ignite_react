8d8fa7624a4efb32a39ae866c2dae09eeef523ef
curtyraissa <raissacurty@gmail.com>
2023-12-15 19:10:15 +0000

    refactor: Apply avatar component to other components

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import {PencilLine} from 'phosphor-react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar';
 
 export function Sidebar(){
     return(
@@ -9,7 +10,7 @@ export function Sidebar(){
         src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         />
         <div className={styles.profile}>
-            <img src='https://avatars.githubusercontent.com/u/117386154?v=4' alt='avatar'/>
+            <Avatar src='https://avatars.githubusercontent.com/u/117386154?v=4' alt='avatar'/>
             <strong>Raissa Curty</strong>
             <span>Web Developer</span>
         </div>
